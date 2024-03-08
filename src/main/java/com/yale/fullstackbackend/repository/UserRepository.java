0530d7c10f0dbc846bb1849c31a29efdc219228c
cbot918 <cbot918@gmail.com>
2024-03-08 15:08:13 +0800

    refactor folder structure: add frontend and reference project

--- src/main/java/com/yale/fullstackbackend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yale.fullstackbackend.repository;
-
-import com.yale.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
